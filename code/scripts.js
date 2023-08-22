d34f2fa8c01ee34bfff6a205ab3be04fe0956497
tungufoss <helgaingim@hi.is>
2023-08-22 12:12:25 +0000

    Refactor code and add collapsible group rows

--- code/scripts.js
+++ code/scripts.js
@@ -1,178 +1,154 @@
-// This function fetches the CSV and populates the table
+// Constants for frequently accessed elements
+const STAFF_TABLE = document.getElementById('staffTable');
+
+// CSV Fetching and Table Populating
 function fetchCSVAndPopulate() {
     fetch('../data/staff.csv')
         .then(response => response.text())
         .then(data => {
-            const rows = data.split('\n').slice(1); // Skip header row
-            const tableBody = document.querySelector("#staffTable tbody");
-
-            rows.forEach(row => {
-                const columns = row.split(',');
-                const tr = document.createElement('tr');
-
-                // Create name column
-                const nameTD = document.createElement('td');
-                nameTD.textContent = columns[0];
-                nameTD.classList.add('bold');  // Apply bold class to name
-                tr.appendChild(nameTD);
-
-                // Create cardiac column
-                const cardiacTD = document.createElement('td');
-                cardiacTD.textContent = columns[1] === "TRUE" ? "Yes" : "No";
-                if (cardiacTD.textContent === "No") {
-                    cardiacTD.classList.add('muted');  // Apply muted class if "No"
-                } else {
-                    cardiacTD.classList.add('cardiac');  // Apply bold class if "Yes"
-                }
-                tr.appendChild(cardiacTD);
-
-                // Create in charge column
-                const inChargeTD = document.createElement('td');
-                inChargeTD.textContent = columns[2].trim() === "TRUE" ? "Yes" : "No";
-                if (inChargeTD.textContent === "No") {
-                    inChargeTD.classList.add('muted');  // Apply muted class if "No"
-                } else {
-                    inChargeTD.classList.add('charge');  // Apply bold class if "Yes"
-                }
-                tr.appendChild(inChargeTD);
-
-                tableBody.appendChild(tr);
-            });
-
-            // Update the counts
+            populateTable(data.split('\n').slice(1)); // Skip header row
             updateCounts();
         });
 }
 
-// Call the function on page load
-document.addEventListener('DOMContentLoaded', fetchCSVAndPopulate);
+function populateTable(rows) {
+    const tableBody = STAFF_TABLE.tBodies[0];
+    rows.forEach(row => {
+        const columns = row.split(',');
+        const tr = document.createElement('tr');
+        tr.appendChild(createTableCell(columns[0], 'bold'));
+        tr.appendChild(createBooleanTableCell(columns[1], 'cardiac'));
+        tr.appendChild(createBooleanTableCell(columns[2], 'charge'));
+        tableBody.appendChild(tr);
+    });
+}
 
+function createTableCell(content, className) {
+    const td = document.createElement('td');
+    td.textContent = content;
+    td.classList.add(className);
+    return td;
+}
 
-// On click of the table header, sort the table
-let sortOrder = 'asc';  // This is used to toggle between ascending and descending order
+function createBooleanTableCell(value, trueClass) {
+    const td = createTableCell(value.trim() === "TRUE" ? "Yes" : "No");
+    if (td.textContent === "Yes") {
+        td.classList.add(trueClass);
+    } else {
+        td.classList.add('muted');
+    }
+    return td;
+}
+
+// Table Sorting
+let sortOrder = 'asc';
 
 function sortTable(columnIndex) {
-    const table = document.getElementById('staffTable');
-    const rows = Array.from(table.tBodies[0].rows);  // Convert rows to an array for easy sorting
-
-    // Determine the sorting order
+    const rows = Array.from(STAFF_TABLE.tBodies[0].rows);
     const orderMultiplier = sortOrder === 'asc' ? 1 : -1;
 
-    // Sort the rows based on the content of the specified column
     const sortedRows = rows.sort((a, b) => {
         const cellA = a.cells[columnIndex].textContent.trim().toLowerCase();
         const cellB = b.cells[columnIndex].textContent.trim().toLowerCase();
-
-        if (cellA < cellB) return -1 * orderMultiplier;
-        if (cellA > cellB) return 1 * orderMultiplier;
-        return 0;
+        return cellA.localeCompare(cellB) * orderMultiplier;
     });
 
-    // Clear the current rows and add the sorted rows
-    table.tBodies[0].innerHTML = '';
-    table.tBodies[0].append(...sortedRows);
+    STAFF_TABLE.tBodies[0].innerHTML = '';
+    STAFF_TABLE.tBodies[0].append(...sortedRows);
 
-    // Toggle the sort order for the next click
     sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
 }
 
-// Calculate and display the counts
+// Count Update
 function updateCounts() {
-    const staffTable = document.getElementById('staffTable');
-
-    const nameCount = staffTable.tBodies[0].rows.length;
-    const cardiacYesCount = Array.from(staffTable.tBodies[0].rows).filter(row => row.cells[1].textContent.trim() === "Yes").length;
-    const chargeYesCount = Array.from(staffTable.tBodies[0].rows).filter(row => row.cells[2].textContent.trim() === "Yes").length;
+    const nameCount = STAFF_TABLE.tBodies[0].rows.length;
+    const cardiacYesCount = Array.from(STAFF_TABLE.tBodies[0].rows).filter(row => row.cells[1].textContent.trim() === "Yes").length;
+    const chargeYesCount = Array.from(STAFF_TABLE.tBodies[0].rows).filter(row => row.cells[2].textContent.trim() === "Yes").length;
 
     document.getElementById("nameCount").textContent = nameCount;
     document.getElementById("cardiacCount").textContent = cardiacYesCount;
     document.getElementById("chargeCount").textContent = chargeYesCount;
 }
 
-// Create dropdown options for the table
-// Fetch the CSV data
-fetch('../data/staff.csv')
-    .then(response => response.text())
-    .then(data => {
-        const lines = data.trim().split("\n");
-        const doctorNames = [];
-
-        // Skip the header line and process the rest
-        for (let i = 1; i < lines.length; i++) {
-            const columns = lines[i].split(",");
-            doctorNames.push(columns[0]);
-        }
-
-        populateDropdowns(doctorNames);
-    });
-
+// Dropdown Handling
 function populateDropdowns(doctorNames) {
-    // Find all dropdowns
     const dropdowns = document.querySelectorAll('.doctor-dropdown');
-
     dropdowns.forEach(dropdown => {
-        // Add an empty default option
-        const defaultOption = document.createElement('option');
-        defaultOption.value = "";
-        defaultOption.textContent = "";
-        dropdown.appendChild(defaultOption);
+        dropdown.appendChild(createOption("", ""));  // Default option
 
-        // Add the doctor names as options
         doctorNames.forEach(name => {
-            const option = document.createElement('option');
-            option.value = name;
-            option.textContent = name;
-            dropdown.appendChild(option);
+            dropdown.appendChild(createOption(name, name));
         });
+
+        handleDropdownChange(dropdown);
     });
 }
 
-document.querySelectorAll('.doctor-dropdown').forEach(dropdown => {
-    let prevValue = dropdown.value; // Store the initial value
+function createOption(value, textContent) {
+    const option = document.createElement('option');
+    option.value = value;
+    option.textContent = textContent;
+    return option;
+}
 
-    // When the dropdown value changes, update the table by disabling the selected option in other dropdowns
+function handleDropdownChange(dropdown) {
+    let prevValue = dropdown.value;
+
     dropdown.addEventListener('change', function () {
         const currentValue = this.value;
-        let columnClass;
-
-        // Identify the column based on class suffix
-        if (this.classList.contains('doctor-dropdown-mon')) {
-            columnClass = 'doctor-dropdown-mon';
-        } else if (this.classList.contains('doctor-dropdown-tue')) {
-            columnClass = 'doctor-dropdown-tue';
-        } else if (this.classList.contains('doctor-dropdown-wed')) {
-            columnClass = 'doctor-dropdown-wed';
-        } else if (this.classList.contains('doctor-dropdown-thu')) {
-            columnClass = 'doctor-dropdown-thu';
-        } else if (this.classList.contains('doctor-dropdown-fri')) {
-            columnClass = 'doctor-dropdown-fri';
-        }
+        const columnClass = getColumnClass(this);
 
         // Re-enable the previous value for other dropdowns in the same column
-        if (prevValue) {
-            document.querySelectorAll(`.${columnClass}`).forEach(innerDropdown => {
-                const option = innerDropdown.querySelector(`option[value="${prevValue}"]`);
-                if (option) {
-                    option.disabled = false;
-                }
-            });
-        }
+        if (prevValue) enableDropdownOption(columnClass, prevValue);
 
-        // If a new value is selected, disable it for other dropdowns in the same column
-        if (currentValue) {
-            document.querySelectorAll(`.${columnClass}`).forEach(innerDropdown => {
-                if (innerDropdown !== this) {
-                    const option = innerDropdown.querySelector(`option[value="${currentValue}"]`);
-                    if (option) {
-                        option.disabled = true;
-                    }
-                }
-            });
-        }
+        // Disable the current value for other dropdowns in the same column
+        if (currentValue) disableDropdownOption(columnClass, currentValue, this);
 
         prevValue = currentValue;
+    });
+}
 
+function getColumnClass(element) {
+    const suffixes = ['mon', 'tue', 'wed', 'thu', 'fri'];
+    for (const suffix of suffixes) {
+        if (element.classList.contains(`doctor-dropdown-${suffix}`)) {
+            return `doctor-dropdown-${suffix}`;
+        }
+    }
+}
 
+function enableDropdownOption(columnClass, value) {
+    toggleDropdownOption(columnClass, value, false);
+}
+
+function disableDropdownOption(columnClass, value, excludingElement) {
+    toggleDropdownOption(columnClass, value, true, excludingElement);
+}
+
+function toggleDropdownOption(columnClass, value, isDisabled, excludingElement) {
+    document.querySelectorAll(`.${columnClass}`).forEach(dropdown => {
+        if (dropdown !== excludingElement) {
+            const option = dropdown.querySelector(`option[value="${value}"]`);
+            if (option) option.disabled = isDisabled;
+        }
+    });
+}
+
+// Content Toggling
+document.querySelectorAll('.group-header').forEach(header => {
+    header.addEventListener('click', () => {
+        let contentRows = header.nextElementSibling;
+        while (contentRows && contentRows.classList.contains('group-content')) {
+            contentRows.classList.toggle('hidden'); // This will add the class if it's not there, and remove it if it is
+            contentRows = contentRows.nextElementSibling;
+        }
     });
 });
 
+// Initial Calls
+document.addEventListener('DOMContentLoaded', () => {
+    fetchCSVAndPopulate();
+    fetch('../data/staff.csv')
+        .then(response => response.text())
+        .then(data => populateDropdowns(data.split('\n').slice(1).map(line => line.split(',')[0])));
+});
