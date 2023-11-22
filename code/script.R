268ffb49711b3b3f5258dcfee16d921157058dc1
tungufoss <helgaingim@hi.is>
2023-11-22 11:06:50 +0000

    Plotting boxplots for the doctors - cf #11

--- code/script.R
+++ code/script.R
@@ -1,40 +1,64 @@
 setwd("C://Users/hbi3/Downloads")
 library(tidyverse)
-library(DBI)
-library(RSQLite)
-db_file = 'database.sqlite'
-file.exists(db_file)
-## connect to db
-con <- dbConnect(drv = RSQLite::SQLite(), dbname = db_file)
 
-## list all tables
-tables <- dbListTables(con)
-print(tables)
+read_database <- function(db_file = 'database.sqlite') {
+  library(DBI)
+  library(RSQLite)
+  if (!file.exists(db_file)) {
+    stop("Database file not found")
+  }
 
-doctors <- dbReadTable(con, "doctors")
-points <- dbGetQuery(con, "
+  print("Connecting to database")
+  con <- dbConnect(drv = RSQLite::SQLite(), dbname = db_file)
+
+  print("Reading tables")
+  tables <- dbListTables(con)
+  print(tables)
+
+  doctors <- dbReadTable(con, "doctors") %>%
+    mutate(roles = ifelse(cardiac & charge, 'Both',
+                          ifelse(cardiac, 'Cardiac',
+                                 ifelse(charge, 'Charge',
+                                        'None')
+                          )))
+
+  points <- dbGetQuery(con, "
   SELECT p.*, period_start, workdays, target_value
   FROM points p
   INNER JOIN schedule ON schedule.id = p.schedule_id
   ") %>%
-  mutate(
-    period_start = as.Date(period_start)
-  ) %>%
-  mutate(regular_week = workdays == 5)
-schedule <- dbReadTable(con, "schedule") %>%
-  mutate(
-    period_start = as.Date(period_start),
-    period_end = as.Date(period_end)
-  )
+    mutate(
+      period_start = as.Date(period_start)
+    ) %>%
+    mutate(regular_week = workdays == 5)
+  schedule <- dbReadTable(con, "schedule") %>%
+    mutate(
+      period_start = as.Date(period_start),
+      period_end = as.Date(period_end)
+    )
 
-dbDisconnect(con)
+  dbDisconnect(con)
 
-schedule %>%
+  return(list(
+    doctors = doctors,
+    points = points,
+    schedule = schedule
+  ))
+}
+
+# Check if data is already loaded
+if (!exists("db")) {
+  print("Loading data")
+  db <- read_database()
+}
+
+
+db$schedule %>%
   ggplot(aes(x = period_start, y = target_value, color = as.factor(workdays))) +
   geom_point() +
   geom_smooth(method = 'lm', formula = 'y~1', se = FALSE) +
   geom_label(
-    data = schedule %>%
+    data = db$schedule %>%
       group_by(workdays) %>%
       summarise(period_start = min(period_start),
                 target_value = mean(target_value),
@@ -43,8 +67,7 @@ schedule %>%
   theme_minimal() +
   xlab(NULL)
 
-points %>% glimpse()
-points %>%
+db$points %>%
   ggplot(aes(x = period_start)) +
   geom_boxplot(aes(group = period_start, y = total_points)) +
   geom_point(aes(y = target_value), color = 'red') +
@@ -52,9 +75,23 @@ points %>%
   xlab(NULL) +
   facet_grid(regular_week ~ .)
 
-points %>%
+db$points %>%
   mutate(off_target = total_points - target_value) %>%
   ggplot(aes(x = period_start)) +
   geom_point(aes(group = period_start, y = off_target, color = abs(off_target))) +
   xlab(NULL) +
   facet_grid(regular_week ~ .)
+
+db$schedule %>%
+  group_by(workdays) %>%
+  tally() %>%
+  print()
+
+# Calculate the means, merge back with the full data, and then plot
+db$points %>%
+  left_join(db$doctors, by = c("doctor_id" = "id")) %>%
+  ggplot(aes(x = reorder(doctor_id, -total_points, FUN=mean), y = total_points, fill = roles)) +
+  geom_boxplot() +
+  xlab(NULL) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) # Rotate x labels for readability
+
